9e701f3f39c4d4010821a25de9547a9811e54e37
Liamlunkamba <liamlunkamba12@gmail.com>
2024-10-07 15:46:30 -0400

    first commit

---
+++ test.py
@@ -0,0 +1,31 @@
+# class Car:
+#     def _init_(self, color,horsepower) -> None:
+#        self.brand = brand
+#        self.color = color
+#        self.horsepower = horsepower
+       
+
+#        def honk(brand):
+#            print(str(brand) + "is honking")
+
+
+#     #    bmw = Car("blue", "blue",400)
+#     #    print(bmw.color)
+#     #    print(bmw.horsepower)
+#     #    bmw.honk("bmw")
+
+#     names = ["Julio", "Edwin", "shiao", "Abdoul","Liam"]
+#     print(names[1:4])
+
+#     for name in names:
+#         print(name)
+
+#         python = "Python is weird"
+#         print(python.find)
+#         print("on" in python)
+
+name = "Liam "
+age = "112 "
+
+print(f"Hello,  {name}!")
+print(f"You are  {age} years old.")
